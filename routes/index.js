9b65bdbba6d30926d4a8df49e17d8f3425930349
Preslav977 <p.cvetanow@gmail.com>
2024-03-14 10:34:55 +0200

    Updated the routers in the index and app

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", (req, res, next) => {
-  console.log(req.url);
-  res.render("index", { title: "Express" });
+router.get("/", (req, res) => {
+  res.redirect("/catalog");
 });
 
 module.exports = router;
